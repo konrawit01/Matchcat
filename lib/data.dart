751ab539e8bb6e3a1b6441f0aacc5cea5ea68a29
konrawit01 <konrawit.ch.62@ubu.ac.th>
2022-03-11 18:25:55 +0700

    Project

---
+++ lib/data.dart
@@ -0,0 +1,83 @@
+import 'package:flip_card/flip_card.dart';
+import 'package:flutter/material.dart';
+
+List<String> fillSourceArray() {
+  return [
+    'assets/animalspics/cat_1.png',
+    'assets/animalspics/cat_1.png',
+    'assets/animalspics/cat_3.png',
+    'assets/animalspics/cat_3.png',
+    'assets/animalspics/cat_5.png',
+    'assets/animalspics/cat_5.png',
+    'assets/animalspics/cat_9.png',
+    'assets/animalspics/cat_9.png',
+    'assets/animalspics/cat_11.png',
+    'assets/animalspics/cat_11.png',
+    'assets/animalspics/cat_13.png',
+    'assets/animalspics/cat_13.png',
+    
+  ];
+}
+enum Level { Hard, Medium, Easy }
+
+
+List getSourceArray(
+  Level level,
+) {
+  List<String> levellist = <String>[];
+  List sourceArray = fillSourceArray();
+  if (level == Level.Hard) {
+    sourceArray.forEach((element) {
+      levellist.add(element);
+    });
+  } else if (level == Level.Medium) {
+    for (int i = 0; i < 6; i++) {
+      levellist.add(sourceArray[i]);
+    }
+  } else if (level == Level.Easy) {
+    for (int i = 0; i < 4; i++) {
+      levellist.add(sourceArray[i]);
+    }
+  }
+
+  levellist.shuffle();
+  return levellist;
+}
+
+
+List<bool> getInitialItemState(Level level) {
+  List<bool> initialItemState = <bool>[];
+  if (level == Level.Hard) {
+    for (int i = 0; i < 12; i++) {
+      initialItemState.add(true);
+    }
+  } else if (level == Level.Medium) {
+    for (int i = 0; i < 6; i++) {
+      initialItemState.add(true);
+    }
+  } else if (level == Level.Easy) {
+    for (int i = 0; i < 4; i++) {
+      initialItemState.add(true);
+    }
+  }
+  return initialItemState;
+}
+
+List<GlobalKey<FlipCardState>> getCardStateKeys(Level level) {
+  List<GlobalKey<FlipCardState>> cardStateKeys =
+      <GlobalKey<FlipCardState>>[];
+  if (level == Level.Hard) {
+    for (int i = 0; i < 12; i++) {
+      cardStateKeys.add(GlobalKey<FlipCardState>());
+    }
+  } else if (level == Level.Medium) {
+    for (int i = 0; i < 6; i++) {
+      cardStateKeys.add(GlobalKey<FlipCardState>());
+    }
+  } else if (level == Level.Easy) {
+    for (int i = 0; i < 4; i++) {
+      cardStateKeys.add(GlobalKey<FlipCardState>());
+    }
+  }
+  return cardStateKeys;
+}
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -3,96 +3,175 @@ import 'flipcardgame.dart';
 import 'data.dart';
 
   class HomePage extends StatefulWidget {
+    const HomePage({Key? key}): super(key: key);
+    
     @override
     State<HomePage> createState() => _HomePageState();
   }
    
   class _HomePageState extends State<HomePage> {
+  
+  var index;
+  List<Widget> get _icons => [];
+  
+ 
+
     @override
     Widget build(BuildContext context) {
       return Scaffold(
-        appBar: AppBar(title: Text('MatchCat'),
-          centerTitle: true,
-          backgroundColor: Colors.indigoAccent),
-            body: ListView.builder(
-                  itemCount: 3, //
-                  itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Stack(
-                          children: [
-                            Container(
-                              height: 90,
-                              width: double.infinity,
-                              decoration: BoxDecoration(color: Colors.green[200],
-                              borderRadius: BorderRadius.circular(30),
-                              boxShadow: [
-                                BoxShadow(
-                                  blurRadius:4,
+       appBar: AppBar(title: Text('MatchCat'),
+        centerTitle: true,
+        backgroundColor: Colors.indigoAccent),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GestureDetector(
+          onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                  builder: (BuildContext context) => _list[index].goto,
+                    ));
+              },
+          child: ListView.builder(
+            itemCount: _list.length, //
+            itemBuilder: (context, index) {
+              return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Stack(
+                    children: [
+                      Container(
+                        height: 100,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                            color: _list[index].primarycolor,//
+                            borderRadius: BorderRadius.circular(30),
+                            boxShadow: [
+                              BoxShadow(
+                                  blurRadius: 4,
                                   color: Colors.black45,
                                   spreadRadius: 0.5,
-                                  offset: Offset(3,4))
-                                ]),
-                            ),
-                            Container(
-                              height: 90,
-                              width: double.infinity,
-                              decoration: BoxDecoration(color: Colors.green[200],
-                              borderRadius: BorderRadius.circular(30),
-                              boxShadow: [
-                                BoxShadow(
-                                  blurRadius:4,
+                                  offset: Offset(3, 4))
+                            ]),
+                      ),
+                      Container(
+                        height: 90,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                            color: _list[index].secomdarycolor,//
+                            borderRadius: BorderRadius.circular(30),
+                            boxShadow: [
+                              BoxShadow(
+                                  blurRadius: 4,
                                   color: Colors.black12,
                                   spreadRadius: 0.3,
-                                  offset: Offset(3,4,))
-                                ]),
-                            ),   
+                                  offset: Offset(
+                                    5,
+                                    3,
+                                  ))
+                            ]),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Center(
+                                child: Text(
+                              _list[index].name,//
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold,
+                                  shadows: [
+                                    Shadow(
+                                      color: Colors.black26,
+                                      blurRadius: 2,
+                                      offset: Offset(1, 2),
+                                    ),
+                                    Shadow(
+                                        color: Colors.green,
+                                        blurRadius: 2,
+                                        offset: Offset(0.5, 2))
+                                  ]),
+                            )),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: genratester(_list[index].onofstar),
+                            )
                           ],
                         ),
-                      );
-                    },
+                      ),
+                    ],
                   ),
-                );
-              }
-            }
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
 
-// list<Widget> genratester(int no) {
-  //List<Widget> icons = [];
-  //for (int i  = 0; i < no; i++){
-    //_icons.insert(
-     // i,
-     // Icon(
-        //Icons.star,
-        //color: Colors.yellow,
-     // )); 
- //}
- //return _icons;
-//}
-// class Details {
-//   String name;
-//   Color primarycolor;
-//   Color secomdarycolor;
-//   Widget goto;
-//   int noOfstar;
-//   Details({this.name, this.primarycolor, this.secomdarycolor});
-// }
 
-// List<Details> _list = [
-//   Details(
-//       name: "EASY",
-//       primarycolor: Colors.green,
-//       secomdarycolor: Colors.green[200]
-//   ),
-//   Details(
-//       name: "MEDIUM",
-//       primarycolor: Colors.orange,
-//       secomdarycolor: Colors.orange[300]
-//   ),
-//   Details(
-//       name: "HARD",
-//       primarycolor: Colors.red,
-//       secomdarycolor: Colors.red[300],
-//   ),
-// ]
+  List<Widget> genratester(int no) {
+    List<Widget> icons = [];
+      for (int i  = 0; i < no; i++){
+        _icons.insert(
+          i,
+          Icon(
+            Icons.star,
+            color: Colors.yellow,
+              )); 
+        }
+        return _icons;
+    }
+}
+
+
+class ListDetails {
+   String name;
+   Color primarycolor;
+   Color secomdarycolor;
+   int onofstar;
+   Widget goto;
+
+  ListDetails({
+    this.name,
+    this.primarycolor,
+    this.secomdarycolor,
+    this.onofstar,
+    this.goto
+  });
+}
+
+
+
+
+List<ListDetails> _list = [
+  ListDetails(
+    name: "EASY",
+    primarycolor: Colors.green,
+    secomdarycolor: Colors.green.shade300, 
+    onofstar: 1, 
+    goto : FlipCardGane(Level.Easy)
+    ),
+    
+  ListDetails(
+    name: "Medium",
+    primarycolor: Colors.green,
+    secomdarycolor: Colors.green.shade300,
+    onofstar: 2, 
+    goto: FlipCardGane(Level.Medium)),
+    
+  ListDetails(
+    name: "Hard",
+    primarycolor: Colors.green,
+    secomdarycolor: Colors.green.shade300,
+    onofstar: 3,
+    goto: FlipCardGane(Level.Hard)
+    ),
+
+];
+
+
+
+
                                                               
                     
